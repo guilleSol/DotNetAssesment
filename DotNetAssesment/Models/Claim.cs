a3ddabff020069a1d6031a34f094a3d6ce39e5d6
German Rodriguez <german.rodriguez@solera.com>
2022-11-18 16:19:24 +0100

    fixed year and date inputs

--- DotNetAssesment/Models/Claim.cs
+++ DotNetAssesment/Models/Claim.cs
@@ -1,10 +1,15 @@
-﻿namespace DotNetAssesment.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetAssesment.Models
 {
     public class Claim
     {
         public int ID { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
+        //From here https://learn.microsoft.com/en-us/aspnet/core/mvc/views/working-with-forms?view=aspnetcore-7.0#the-input-tag-helper
+        //So it does not display the time, just the date
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public int VehicleID { get; set; }
         //Allow this property to be nullable, so the form does not complain
